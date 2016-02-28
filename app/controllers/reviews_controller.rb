9605287bcef0eb4ce519ede537f81716379e8ca4
rooserik <rooserik@gmail.com>
2016-02-29 00:41:25 +0100

    added index and new tests and methods for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :set_area
+  def index
+    render json:{
+      reviews: @area.reviews.order(id: :desc)
+    }
+  end
+
+
+  def new
+    render json:{task: @area.reviews.new}
+  end
+
+  private
+
+  def set_area
+    @area=Area.find(params[:area_id])
+  end
+  def review_params
+    params.require(:review).permit(:story, :writer, :area_id)
+  end
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsController, type: :controller do
+  review=FactoryGirl.create(:review)
 
+  describe 'reviews#index' do
+    it "gets reviews index" do
+      get :index, :format => :json, :area_id => review.area.id
+      expect(response).to be_success
+    end
+  end
+
+  describe '#new' do
+    it "gets new_review" do
+      get :new, :format => :json, :area_id => review.area.id
+      expect(response).to be_success
+    end
+  end
 end
